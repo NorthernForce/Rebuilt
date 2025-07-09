560cfc8395b645128beed47df437e13b1d14345a
EverettHThe1 <139522291+Daboss-1@users.noreply.github.com>
2025-07-09 19:30:58 -0400

    Add vision subsystem (#7)
    * zippify
    
    * nfr generic container
    
    * implement generic
    
    * bindOI
    
    * bind zippy container, not generic
    
    * remove unused imports
    
    * move tuner constants
    
    * add limelight and photonvision
    
    * add javadocs
    
    * add correct determination of the presence of the limelight pose estimate.
    
    * correctly handle field layout errors
    
    * change pose validity
    
    * messed up comment
    
    * remove useless subsystem base class
    
    * welp, looks like io is nice 😠
    
    * use units
    
    * FIx Connor fixes
    
    * build code
    
    * change to getPoses 😡😡😡
    
    * throw stuff
    
    ---------
    
    Co-authored-by: michellebourget <michelle.bourget@gorhamschools.org>

--- src/main/java/frc/robot/zippy/ZippyContainer.java
+++ src/main/java/frc/robot/zippy/ZippyContainer.java
@@ -6,32 +6,53 @@ package frc.robot.zippy;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.List;
+
 import org.northernforce.util.NFRRobotContainer;
+import org.photonvision.simulation.SimCameraProperties;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.FollowPathCommand;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.LimelightHelpers;
 import frc.robot.Telemetry;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.apriltagvision.*;
 import frc.robot.zippy.generated.ZippyTunerConstants;
 
 public class ZippyContainer implements NFRRobotContainer
 {
     private double MaxSpeed = ZippyTunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
                                                                                        // speed
-
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    public final CommandSwerveDrivetrain drivetrain = ZippyTunerConstants.createDrivetrain();
+    private final CommandSwerveDrivetrain drivetrain = ZippyTunerConstants.createDrivetrain();
+
+    private final AprilTagVisionIO aprilTagVisionIO;
 
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
 
     public ZippyContainer()
     {
+        if (RobotBase.isSimulation())
+        {
+            aprilTagVisionIO = new AprilTagVisionIOPhotonVisionSim(
+                    ZippyConstants.VisionConstants.LimeLightConstants.kLimeLightName, new SimCameraProperties(),
+                    ZippyConstants.VisionConstants.PhotonVisionConstants.kRobotToCamera);
+        } else
+        {
+            aprilTagVisionIO = new AprilTagVisionIOLimelight(
+                    ZippyConstants.VisionConstants.LimeLightConstants.kLimeLightName);
+            LimelightHelpers.SetFiducialIDFiltersOverride(
+                    ZippyConstants.VisionConstants.LimeLightConstants.kLimeLightName,
+                    ZippyConstants.VisionConstants.LimeLightConstants.kValidIds);
+        }
         autoChooser = AutoBuilder.buildAutoChooser("Tests");
         SmartDashboard.putData("Auto Mode", autoChooser);
 
@@ -40,6 +61,23 @@ public class ZippyContainer implements NFRRobotContainer
         // Warmup PathPlanner to avoid Java pauses
         FollowPathCommand.warmupCommand().schedule();
         drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    @Override
+    public void periodic()
+    {
+        if (RobotBase.isSimulation())
+        {
+            AprilTagVisionIOPhotonVisionSim.getVisionSystemSim().update(drivetrain.getState().Pose);
+        }
+        List<Pose2dWithTimestamp> poses = aprilTagVisionIO.getPoses();
+        Rotation2d robotYaw = drivetrain.getState().Pose.getRotation();
+        aprilTagVisionIO.setHeading(robotYaw, Rotation2d.fromDegrees(0));
+        for (Pose2dWithTimestamp pose : poses)
+        {
+            drivetrain.addVisionMeasurement(pose.pose(), pose.timestamp(), ZippyConstants.VisionConstants.kStdDevs);
+        }
+
     }
 
     public Command getAutonomousCommand()
