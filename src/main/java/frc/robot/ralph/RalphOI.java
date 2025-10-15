f84eaaf5112605f361eba3d15457909f5bf02ebc
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2025-10-14 20:10:12 -0400

    formatting

--- src/main/java/frc/robot/ralph/RalphOI.java
+++ src/main/java/frc/robot/ralph/RalphOI.java
@@ -1,7 +1,6 @@
 package frc.robot.ralph;
 
 import java.util.function.DoubleSupplier;
-import java.util.function.Function;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,7 +20,7 @@ public class RalphOI
     {
         var driveController = new CommandXboxController(0);
         var manipulatorController = new CommandXboxController(1);
-        
+
         container.getDrive()
                 .setDefaultCommand(container.getDrive().driveByJoystick(inputProc(driveController::getLeftX),
                         inputProc(driveController::getLeftY), inputProc(driveController::getRightX)));
