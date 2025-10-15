65d874dc2aeb22c1461d673f2eda647235de9f12
Paradox0100 <milo.saucier@gmail.com>
2025-10-15 19:46:32 -0400

    it compiled soo...

--- src/main/java/frc/robot/zippy/ZippyConstants.java
+++ src/main/java/frc/robot/zippy/ZippyConstants.java
@@ -5,6 +5,9 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N3;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Rotations;
+import edu.wpi.first.units.measure.Angle;
 
 public class ZippyConstants
 {
@@ -24,5 +27,17 @@ public class ZippyConstants
             public static final Transform3d kRobotToCamera = new Transform3d(new Translation3d(0.0, 0.0, 0.0),
                     new Rotation3d(0.0, 0.0, 0.0));
         }
+    }
+
+    public static class ClimberConstants
+    {
+        public static final int kId = 17;
+        public static final double kEncoderId = 23;
+        public static final boolean kInverted = false;
+        public static final double kGearRatio = 100.0;
+        public static final Angle kLowerLimit = Rotations.of(-0.03);
+        public static final Angle kUpperLimit = Rotations.of(0.22);
+        public static final Angle kSweetAngle = Degrees.of(70.0);
+        public static final double kClimbSpeed = 1;
     }
 }
