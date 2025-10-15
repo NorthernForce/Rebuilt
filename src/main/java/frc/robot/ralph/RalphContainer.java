d54a425be29192e0b861452c344bd560331f4d1d
Kasey N. <148725607+kcnan@users.noreply.github.com>
2025-10-15 17:41:01 -0400

    Starting point for drive code (#27)
    * a good starting point
    
    * changes to pass the connor checker
    
    * formatting
    
    * add shuffleboard command to reset encoders
    
    * set perspective in driveByJoystick
    
    * add + map command to reset orientation

--- src/main/java/frc/robot/ralph/RalphContainer.java
+++ src/main/java/frc/robot/ralph/RalphContainer.java
@@ -2,11 +2,45 @@ package frc.robot.ralph;
 
 import org.northernforce.util.NFRRobotContainer;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.ralph.generated.RalphTunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RalphContainer implements NFRRobotContainer
 {
+    private final CommandSwerveDrivetrain drive;
+    private final Field2d field;
+
+    public RalphContainer()
+    {
+        drive = new CommandSwerveDrivetrain(RalphTunerConstants.DrivetrainConstants,
+                RalphConstants.DrivetrainConstants.kMaxSpeed, RalphConstants.DrivetrainConstants.kMaxAngularSpeed,
+                RalphTunerConstants.FrontLeft, RalphTunerConstants.FrontRight, RalphTunerConstants.BackLeft,
+                RalphTunerConstants.BackRight);
+        field = new Field2d();
+        Shuffleboard.getTab("Developer").add(field);
+        Shuffleboard.getTab("Developer").add("Reset Encoders", drive.resetEncoders());
+        Shuffleboard.getTab("Developer").add("Reset Orientation", drive.resetOrientation());
+    }
+
+    /**
+     * gets the drive subsystem
+     *
+     * @return the drive subsystem
+     */
+    public CommandSwerveDrivetrain getDrive()
+    {
+        return drive;
+    }
+
+    @Override
+    public void periodic()
+    {
+        field.setRobotPose(getDrive().getState().Pose);
+    }
 
     @Override
     public void bindOI()
