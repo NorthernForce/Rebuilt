ae77081197057497836736a00f89c7a95704c700
Daboss-1 <everett.hurder@gmail.com>
2026-02-09 18:46:20 -0500

    limelight detects correct pose

--- src/main/java/frc/robot/lobby/LobbyConstants.java
+++ src/main/java/frc/robot/lobby/LobbyConstants.java
@@ -59,7 +59,7 @@ public class LobbyConstants
         {
             public static final String kLimeLightName = "limelight";
             public static final int[] kValidIds =
-            { 6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22 };
+            { 6, 7, 8, 9, 10, 11, 15, 17, 18, 19, 20, 21, 22 };
         }
 
         public class PhotonVisionConstants
