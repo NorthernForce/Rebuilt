bcdf90767d31734880145529b9f2070d440a37ec
Zachary Bourgoin <whiteheatzack@gmail.com>
2026-02-10 07:53:02 -0500

    Revert "Add Turret (#97)"
    This reverts commit d49d6fbd685196c5f04885c7480c7681c8cd03ab.

--- src/main/java/frc/robot/FieldConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-
-public class FieldConstants
-{
-    public static final Pose2d HubPosition = new Pose2d(4.63, 4.03, new Rotation2d(0));
-}
