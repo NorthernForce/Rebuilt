87573e357b43e83f559f6d25d10dc95fb6e303b4
Daboss-1 <everett.hurder@gmail.com>
2026-02-25 18:33:03 -0500

    test mode starter pack

--- src/main/java/frc/robot/lobby/LobbyOI.java
+++ src/main/java/frc/robot/lobby/LobbyOI.java
@@ -10,6 +10,7 @@ import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -17,62 +18,70 @@ import frc.robot.lobby.subsystems.spindexer.commands.RunSpindexer;
 import frc.robot.lobby.subsystems.turret.commands.PrepTurretCommand;
 import frc.robot.lobby.subsystems.turret.commands.PrepTurretWithValues;
 
-public class LobbyOI
-{
-    private static DoubleSupplier inputProc(DoubleSupplier input)
-    {
-        return () ->
-        {
-            double x = MathUtil.applyDeadband(input.getAsDouble(), 0.1, 1);
-            return -x * Math.abs(x);
-        };
-    }
+public class LobbyOI {
+        private static DoubleSupplier inputProc(DoubleSupplier input) {
+                return () -> {
+                        double x = MathUtil.applyDeadband(input.getAsDouble(), 0.1, 1);
+                        return -x * Math.abs(x);
+                };
+        }
 
-    public void bind(LobbyContainer container)
-    {
-        var driveController = new CommandXboxController(0);
-        var manipulatorController = new CommandXboxController(1);
+        public void bind(LobbyContainer container) {
+                var driveController = new CommandXboxController(0);
+                var manipulatorController = new CommandXboxController(1);
+                var drive = container.getDrive();
+                var intake = container.getIntake();
+                if (DriverStation.isTest()) { // checks if we are in test mode
+                        driveController.rightTrigger().whileTrue(intake.intake(1));
+                        driveController.rightBumper().whileTrue(Commands.run(() -> container.getSpindexer().getCarousel().setPower(1), container.getSpindexer()));
+                        container.getTurret().getShooter().start();
+                } else {
 
-        var drive = container.getDrive();
-        var intake = container.getIntake();
+                        drive.setDefaultCommand(drive.driveByJoystick(inputProc(driveController::getLeftY),
+                                        inputProc(driveController::getLeftX), inputProc(driveController::getRightX)));
+                        driveController.back().onTrue(drive.resetOrientation());
 
-        drive.setDefaultCommand(drive.driveByJoystick(inputProc(driveController::getLeftY),
-                inputProc(driveController::getLeftX), inputProc(driveController::getRightX)));
-        driveController.back().onTrue(drive.resetOrientation());
+                        // Pass turret's hood constants for danger zone calculation
+                        var hood = container.getTurret().getHood();
+                        // container.getTurret().setDefaultCommand(container.getTurret().runBasedOnLocation(()
+                        // -> drive.getState().Pose,
+                        // hood.getDangerZone(), hood.getTrenchPositions()));
 
-        // Pass turret's hood constants for danger zone calculation
-        var hood = container.getTurret().getHood();
-        // container.getTurret().setDefaultCommand(container.getTurret().runBasedOnLocation(()
-        // -> drive.getState().Pose,
-        // hood.getDangerZone(), hood.getTrenchPositions()));
+                        // Log when trigger is pressed to verify controller works
+                        driveController.a().onTrue(Commands.runOnce(() -> container
+                                        .resetOdometry(new Pose2d(Meters.of(0), Meters.of(0),
+                                                        new Rotation2d(Degrees.of(180))))));
 
-        // Log when trigger is pressed to verify controller works
-        driveController.a().onTrue(Commands.runOnce(() -> container
-                .resetOdometry(new Pose2d(Meters.of(0), Meters.of(0), new Rotation2d(Degrees.of(180))))));
+                        manipulatorController.b().onTrue(Commands.runOnce(() -> {
+                                DogLog.log("Turret/csvValue", container.getTurret().getHoodTargetingCalculator()
+                                                .getValueForDistance(5.0));
+                        }));
+                        (new Trigger(() -> container.getSpindexer().getJammed())).whileTrue(
+                                        Commands.defer(() -> container.getSpindexer().runBackwards(),
+                                                        Set.of(container.getSpindexer())));
+                        driveController.rightTrigger().whileTrue(new RunSpindexer(container.getSpindexer()));
+                        // driveController.leftTrigger().whileTrue(new
+                        // PrepTurretWithValues(container.getTurret()));
+                        driveController.povUp()
+                                        .onTrue(Commands.runOnce(() -> hood.setTargetAngle(Degrees.of(180)),
+                                                        container.getTurret()))
+                                        .onFalse(Commands.runOnce(() -> hood.setTargetAngle(Degrees.zero()),
+                                                        container.getTurret()));
+                        // manipulatorController.leftTrigger().whileTrue(intake.getRunToIntakeAngleCommand());
+                        // intake.setDefaultCommand(intake.getRunToStowAngleCommand());
+                        manipulatorController.leftBumper().whileTrue(intake.intake(0.75)).onFalse(intake.stopIntake());
+                        manipulatorController.leftTrigger().whileTrue(new PrepTurretWithValues(container.getTurret()));
+                        manipulatorController.rightTrigger().whileTrue(new RunSpindexer(container.getSpindexer()));
+                        driveController.leftTrigger().whileTrue(
+                                        container.roughDriveToPose(new Pose2d(Meters.of(3), Meters.of(3),
+                                                        new Rotation2d(Degrees.of(0)))));
+                        driveController.rightTrigger().whileTrue(
+                                        container.driveToPose(new Pose2d(Meters.of(3), Meters.of(3),
+                                                        new Rotation2d(Degrees.of(0)))));
 
-        manipulatorController.b().onTrue(Commands.runOnce(() ->
-        {
-            DogLog.log("Turret/csvValue", container.getTurret().getHoodTargetingCalculator().getValueForDistance(5.0));
-        }));
-        (new Trigger(() -> container.getSpindexer().getJammed())).whileTrue(
-                Commands.defer(() -> container.getSpindexer().runBackwards(), Set.of(container.getSpindexer())));
-        driveController.rightTrigger().whileTrue(new RunSpindexer(container.getSpindexer()));
-        // driveController.leftTrigger().whileTrue(new
-        // PrepTurretWithValues(container.getTurret()));
-        driveController.povUp()
-                .onTrue(Commands.runOnce(() -> hood.setTargetAngle(Degrees.of(180)), container.getTurret()))
-                .onFalse(Commands.runOnce(() -> hood.setTargetAngle(Degrees.zero()), container.getTurret()));
-        // manipulatorController.leftTrigger().whileTrue(intake.getRunToIntakeAngleCommand());
-        // intake.setDefaultCommand(intake.getRunToStowAngleCommand());
-        manipulatorController.leftBumper().whileTrue(intake.intake(0.75)).onFalse(intake.stopIntake());
-        manipulatorController.leftTrigger().whileTrue(new PrepTurretWithValues(container.getTurret()));
-        manipulatorController.rightTrigger().whileTrue(new RunSpindexer(container.getSpindexer()));
-        driveController.leftTrigger().whileTrue(
-                container.roughDriveToPose(new Pose2d(Meters.of(3), Meters.of(3), new Rotation2d(Degrees.of(0)))));
-        driveController.rightTrigger().whileTrue(
-                container.driveToPose(new Pose2d(Meters.of(3), Meters.of(3), new Rotation2d(Degrees.of(0)))));
-
-        driveController.a().onTrue(Commands.runOnce(() -> container
-                .resetOdometry(new Pose2d(Meters.of(0), Meters.of(0), new Rotation2d(Degrees.of(180))))));
-    }
+                        driveController.a().onTrue(Commands.runOnce(() -> container
+                                        .resetOdometry(new Pose2d(Meters.of(0), Meters.of(0),
+                                                        new Rotation2d(Degrees.of(180))))));
+                }
+        }
 }
