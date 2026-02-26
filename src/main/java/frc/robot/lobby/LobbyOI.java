308f46f4664da6b174228f5d42ff75edd84ac8d2
Daboss-1 <everett.hurder@gmail.com>
2026-02-26 00:12:41 -0500

    add povleft and right

--- src/main/java/frc/robot/lobby/LobbyOI.java
+++ src/main/java/frc/robot/lobby/LobbyOI.java
@@ -37,11 +37,14 @@ public class LobbyOI
         var manipulatorController = new CommandXboxController(1);
         var drive = container.getDrive();
         var intake = container.getIntake();
+
         if (DriverStation.isTest())
         {
             driveController.rightBumper().whileTrue(
                     Commands.run(() -> container.getSpindexer().getCarousel().setPower(1), container.getSpindexer()));
+
             container.getTurret().getShooter().start();
+
             driveController.rightTrigger().whileTrue(Commands
                     .run(() -> container.getSpindexer().getFlicker().setPower(1), container.getTurret()).alongWith(
                             Commands.run(() -> container.getSpindexer().getCarousel().setPower(1),
@@ -54,30 +57,35 @@ public class LobbyOI
                                                     container.getSpindexer()),
                                             Commands.run(() -> container.getTurret().getShooter().stop(),
                                                     container.getTurret())));
+
             drive.setDefaultCommand(drive.driveByJoystick(inputProc(driveController::getLeftY),
                     inputProc(driveController::getLeftX), inputProc(driveController::getRightX)));
+
             driveController.povUp().whileTrue(Commands.run(() -> container.getTurret().getHood().setSpeed(0.2, false)))
                     .onFalse(Commands.runOnce(() -> container.getTurret().getHood().setSpeed(0, false)));
+
             driveController.povDown()
                     .whileTrue(Commands.run(() -> container.getTurret().getHood().setSpeed(-0.2, false)))
                     .onFalse(Commands.runOnce(() -> container.getTurret().getHood().setSpeed(0, false)));
+
             driveController.leftBumper().whileTrue(container.getIntake().getRunToIntakeAngleCommand())
                     .onFalse(container.getIntake().getRunToStowAngleCommand());
 
+            driveController.povLeft().whileTrue(container.getTurret().getSuzie().setSpeed(0.05))
+                    .onFalse(container.getTurret().getSuzie().setSpeed(0));
+
+            driveController.povRight().whileTrue(container.getTurret().getSuzie().setSpeed(-0.05))
+                    .onFalse(container.getTurret().getSuzie().setSpeed(0));
+
         } else
         {
-
             drive.setDefaultCommand(drive.driveByJoystick(inputProc(driveController::getLeftY),
                     inputProc(driveController::getLeftX), inputProc(driveController::getRightX)));
+
             driveController.back().onTrue(drive.resetOrientation());
 
-            // Pass turret's hood constants for danger zone calculation
             var hood = container.getTurret().getHood();
-            // container.getTurret().setDefaultCommand(container.getTurret().runBasedOnLocation(()
-            // -> drive.getState().Pose,
-            // hood.getDangerZone(), hood.getTrenchPositions()));
 
-            // Log when trigger is pressed to verify controller works
             driveController.a().onTrue(Commands.runOnce(() -> container
                     .resetOdometry(new Pose2d(Meters.of(0), Meters.of(0), new Rotation2d(Degrees.of(180))))));
 
@@ -86,16 +94,13 @@ public class LobbyOI
                 DogLog.log("Turret/csvValue",
                         container.getTurret().getHoodTargetingCalculator().getValueForDistance(5.0));
             }));
+
             (new Trigger(() -> container.getSpindexer().getJammed())).whileTrue(
                     Commands.defer(() -> container.getSpindexer().runBackwards(), Set.of(container.getSpindexer())));
 
-            // driveController.leftTrigger().whileTrue(new
-            // PrepTurretWithValues(container.getTurret()));
             driveController.povUp()
                     .onTrue(Commands.runOnce(() -> hood.setTargetAngle(Degrees.of(180)), container.getTurret()))
                     .onFalse(Commands.runOnce(() -> hood.setTargetAngle(Degrees.zero()), container.getTurret()));
-            // manipulatorController.leftTrigger().whileTrue(intake.getRunToIntakeAngleCommand());
-            // intake.setDefaultCommand(intake.getRunToStowAngleCommand());
 
             manipulatorController.b().onTrue(Commands.runOnce(() ->
             {
@@ -105,25 +110,14 @@ public class LobbyOI
 
             driveController.rightTrigger().whileTrue(
                     new RunSpindexer(container.getSpindexer(), LobbyConstants.SpindexerConstants.kDeJamTime));
-            // driveController.leftTrigger().whileTrue(new
-            // PrepTurretWithValues(container.getTurret()));
-            // driveController.povUp()
-            // .onTrue(Commands.runOnce(() -> hood.setTargetAngle(Degrees.of(180)),
-            // container.getTurret()))
-            // .onFalse(Commands.runOnce(() -> hood.setTargetAngle(Degrees.zero()),
-            // container.getTurret()));
-            // manipulatorController.leftTrigger().whileTrue(intake.getRunToIntakeAngleCommand());
-            // intake.setDefaultCommand(intake.getRunToStowAngleCommand());
+
             driveController.leftTrigger().whileTrue(intake.intake(0.75)).onFalse(intake.stopIntake());
 
             container.getTurret().getShooter().setDefaultCommand(container.getTurret().getShooter().stop());
-            // manipulatorController.rightTrigger().whileTrue(new
-            // RunSpindexer(container.getSpindexer()));
-            // manipulatorController.leftBumper().whileTrue(new
-            // PrepTurretWithValues(container.getTurret()));
 
             driveController.povLeft().whileTrue(container.getTurret().getSuzie().setSpeed(0.05))
                     .onFalse(container.getTurret().getSuzie().setSpeed(0));
+
             driveController.povRight().whileTrue(container.getTurret().getSuzie().setSpeed(-0.05))
                     .onFalse(container.getTurret().getSuzie().setSpeed(0));
         }
