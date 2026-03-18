07ab9a58fc3b4a07ac58161a189a496dd0c6e9c9
Daboss-1 <everett.hurder@gmail.com>
2026-03-18 13:08:16 -0400

    tmp

--- src/main/java/frc/robot/lobby/LobbyContainer.java
+++ src/main/java/frc/robot/lobby/LobbyContainer.java
@@ -263,8 +263,10 @@ public class LobbyContainer implements NFRRobotContainer
             turret.getSuzie().resetEncoders();
         }));
         DashboardSystem turntableSystem = dashboard.putSystem("Turntable");
-        turntableSystem.withCommand("Turntable SysId Quasistatic Forward", turret.getSuzie().getSysIdQuasistaticForward());
-        turntableSystem.withCommand("Turntable SysId Quasistatic Reverse", turret.getSuzie().getSysIdQuasistaticReverse());
+        turntableSystem.withCommand("Turntable SysId Quasistatic Forward",
+                turret.getSuzie().getSysIdQuasistaticForward());
+        turntableSystem.withCommand("Turntable SysId Quasistatic Reverse",
+                turret.getSuzie().getSysIdQuasistaticReverse());
         turntableSystem.withCommand("Turntable SysId Dynamic Forward", turret.getSuzie().getSysIdDynamicForward());
         turntableSystem.withCommand("Turntable SysId Dynamic Reverse", turret.getSuzie().getSysIdDynamicReverse());
     }
